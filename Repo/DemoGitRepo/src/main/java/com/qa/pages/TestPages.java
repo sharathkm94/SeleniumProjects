a854ce30d2ad377655887a71a5edf825763e3b70
Nagabhushan.M N <Nagabhushan.M N@hp>
2019-11-03 13:38:11 +0530

    Moved from WD to LD

---
+++ Repo/DemoGitRepo/src/main/java/com/qa/pages/TestPages.java
@@ -0,0 +1,10 @@
+package com.qa.pages;
+
+public class TestPages 
+{
+	public void page()
+	{
+		System.out.println("------------");
+	}
+
+}
---
+++ Repo/DemoGitRepo/src/test/java/com/qa/tests/Test.java
@@ -0,0 +1,10 @@
+package com.qa.tests;
+
+public class Test 
+{
+	public void test1()
+	{
+		System.out.println("------------");
+	}
+
+}
